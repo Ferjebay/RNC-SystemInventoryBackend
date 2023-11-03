4732055e1bee2969afe4dfd2fddbf80eb8f020da
FernanDev <juan96saa@gmail.com>
2023-11-03 17:56:49 -0500

    correccion ruta api x4

---
+++ public/assets/auth-user.5eae0586.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.9a69bdf9.js";const r=t("auth/user",{state:()=>({token:"",modeDark:!0}),persist:!0,getters:{},actions:{setToken(e){this.token=e},setModeDark(e){this.modeDark=e}}});export{r as u};
--- public/assets/use-quasar.b99de7bf.js
+++
@@ -1,1 +0,0 @@
-import{k as a,q as r}from"./index.d36c9ddb.js";function u(){return a(r)}export{u};
