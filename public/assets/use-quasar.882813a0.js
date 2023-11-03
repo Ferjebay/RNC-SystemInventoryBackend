bd9fe591424cf88d19c226723b0a81db61976201
FernanDev <juan96saa@gmail.com>
2023-11-03 18:35:46 -0500

    correccion ruta api x5

--- public/assets/use-quasar.882813a0.js
+++
@@ -1,1 +0,0 @@
-import{k as a,q as r}from"./index.9a69bdf9.js";function u(){return a(r)}export{u};
---
+++ public/assets/use-quasar.a4e78766.js
@@ -0,0 +1,1 @@
+import{k as a,q as r}from"./index.f22722bd.js";function u(){return a(r)}export{u};
