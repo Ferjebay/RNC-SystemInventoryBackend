c62f5661f1b66cd1e93176c482dff1e42107361d
JohnyAngoGMT <159568307+JohnyAngoGMT@users.noreply.github.com>
2024-05-04 10:15:30 -0500

    fix msg ws

--- public/assets/useRolPermisos.5a3a921f.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./use-quasar.f2cac59d.js";const u=()=>{const s=e();return{validarPermisos:r=>!!(s.permisos!==null&&(s.permisos.includes(r)||r=="sin-permiso"))}};export{u};
---
+++ public/assets/useRolPermisos.87ae6fbf.js
@@ -0,0 +1,1 @@
+import{u as e}from"./use-quasar.dbe3b2af.js";const u=()=>{const s=e();return{validarPermisos:r=>!!(s.permisos!==null&&(s.permisos.includes(r)||r=="sin-permiso"))}};export{u};
