c14cfdeadec546c59244d3fead418140d5213df0
JohnyAngoGMT <159568307+JohnyAngoGMT@users.noreply.github.com>
2024-05-04 09:55:04 -0500

    fix error domain

---
+++ public/assets/useRolPermisos.5a3a921f.js
@@ -0,0 +1,1 @@
+import{u as e}from"./use-quasar.f2cac59d.js";const u=()=>{const s=e();return{validarPermisos:r=>!!(s.permisos!==null&&(s.permisos.includes(r)||r=="sin-permiso"))}};export{u};
--- public/assets/useRolPermisos.f8ea3918.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./use-quasar.0a9574a2.js";const u=()=>{const s=e();return{validarPermisos:r=>!!(s.permisos!==null&&(s.permisos.includes(r)||r=="sin-permiso"))}};export{u};
