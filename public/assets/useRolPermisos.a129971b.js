1efdbf4dec452e3e820934a7d87aaf5e88a6f79e
JohnyAngoGMT <159568307+JohnyAngoGMT@users.noreply.github.com>
2024-07-03 22:51:16 -0500

    correcion de error de etiqueta img

--- public/assets/useRolPermisos.a129971b.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./use-quasar.48a562df.js";const u=()=>{const s=e();return{validarPermisos:r=>!!(s.permisos!==null&&(s.permisos.includes(r)||r=="sin-permiso"))}};export{u};
---
+++ public/assets/useRolPermisos.ddd328c4.js
@@ -0,0 +1,1 @@
+import{u as e}from"./use-quasar.a4369af7.js";const u=()=>{const s=e();return{validarPermisos:r=>!!(s.permisos!==null&&(s.permisos.includes(r)||r=="sin-permiso"))}};export{u};
